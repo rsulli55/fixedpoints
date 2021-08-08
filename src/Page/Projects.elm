900f6daebb16c162ba0605c5a00e63f330617e9c
Ryan Sullivant <rsulli55@gmail.com>
2021-08-08 11:48:59 -0700

    Update Projects page

--- src/Page/Projects.elm
+++ src/Page/Projects.elm
@@ -1,16 +1,5 @@
 module Page.Projects exposing (Data, Model, Msg, page)
 
--- import Element.Background as Background
--- import Element.Border as Border
--- import Element.Font as Font
--- import Element.Input as Input
--- import MdRendering
--- import OptimizedDecoder as Decode
--- import Secrets
--- import Url.Builder as UrlBuilder
--- import Constants exposing (..)
--- import Element exposing (Element)
-
 import Browser.Navigation
 import Css
 import DataSource exposing (DataSource)
@@ -34,103 +23,15 @@ import Tailwind.Utilities as Tw
 import View exposing (View)
 
 
-
--- type alias Project =
---     { repo : Repo
---     , details : String
---     , expanded : Bool
---     }
-
-
-type alias RepoDetails =
-    String
-
-
-
--- projectNames : DataSource (List String)
--- projectNames =
---     Glob.succeed (\capture -> capture)
---         -- |> Glob.captureFilePath
---         |> Glob.match (Glob.literal "content/projects/")
---         |> Glob.capture Glob.wildcard
---         |> Glob.match (Glob.literal ".md")
---         |> Glob.toDataSource
---         |> DataSource.map (Debug.log "ProjectNames")
-
-
 projectRepos : DataSource (List GithubRepo)
 projectRepos =
     DataSource.map GithubRepo.getRepos projectNames
         |> DataSource.resolve
 
 
-
--- |> DataSource.map (Debug.log "projectRepos")
-
-
 getFilePath : String -> String
 getFilePath name =
     "content/projects/" ++ name ++ ".md"
-
-
-
--- getDetail : String -> DataSource String
--- getDetail name =
---     DataSource.File.bodyWithoutFrontmatter (getFilePath name)
--- projectDetails : DataSource (List String)
--- projectDetails =
---     DataSource.map (List.map getDetail) projectNames
---         |> DataSource.resolve
---         |> DataSource.map (Debug.log "projectDetails")
--- combineRepoAndDetails : List Repo -> List String -> List Project
--- combineRepoAndDetails =
---     List.map2 (\repo details -> { repo = repo, details = details, expanded = False })
--- projects : DataSource (List Project)
--- projects =
---     DataSource.map2 combineRepoAndDetails projectRepos projectDetails
--- projectDetailsButton : Project -> Model -> Element Msg
--- projectDetailsButton project model =
---     let
---         expanded =
---             case Dict.get project.repo.name model.expanded of
---                 Just bool ->
---                     bool
---                 Nothing ->
---                     False
---     in
---     let
---         buttonText =
---             if expanded then
---                 "Collapse Details ⤴"
---             else
---                 "Expand Details ⤵"
---     in
---     let
---         button =
---             Input.button
---                 [ Background.color colorPrimary
---                 , Element.mouseOver [ Background.color colorPrimaryHighlight ]
---                 , Border.rounded 5
---                 , Element.alignRight
---                 ]
---                 { onPress = Just (Toggle project.repo.name)
---                 , label =
---                     Element.text buttonText
---                 }
---     in
---     let
---         renderedMd =
---             if expanded then
---                 Element.paragraph [ Background.color colorMedGrey, Element.width Element.fill ]
---                     [ MdRendering.renderMd project.details ]
---             else
---                 Element.none
---     in
---     Element.column
---         [ Element.spacing 10
---         , Element.width Element.fill
---         ]
---         [ button, renderedMd ]
 
 
 displayRepo : GithubRepo -> Html Msg
@@ -217,18 +118,6 @@ displayRepos repos =
             repos
 
 
-
--- Element.column
---     [ Element.width Element.fill
---     , Element.spacing 15
---     , Element.paddingXY 25 5
---     ]
---     (List.map
---         (displayProject model)
---         projs
---     )
-
-
 type alias Model =
     ()
 
@@ -264,17 +153,17 @@ head :
     -> List Head.Tag
 head static =
     Seo.summary
-        { canonicalUrlOverride = Nothing
-        , siteName = "elm-pages"
+        { canonicalUrlOverride = Just "https://fixedpoints.xyz"
+        , siteName = "fixedpoints"
         , image =
-            { url = Pages.Url.external "TODO"
-            , alt = "elm-pages logo"
+            { url = Pages.Url.fromPath (Path.fromString "public/images/logo.svg")
+            , alt = "fixedpoints logo"
             , dimensions = Nothing
             , mimeType = Nothing
             }
-        , description = "TODO"
+        , description = "Projects - Fixed Points"
         , locale = Nothing
-        , title = "TODO title" -- metadata.title -- TODO
+        , title = "Projects - Fixed Points" -- metadata.title -- TODO
         }
         |> Seo.website
 
@@ -285,111 +174,9 @@ view :
     -> StaticPayload Data RouteParams
     -> View Msg
 view maybeUrl sharedModel static =
-    { title = "Fixed Points - Personal Projects"
+    { title = "Projects - Fixed Points"
     , body =
         [ Html.span [ css [ Tw.flex, Tw.justify_center, Tw.text_3xl ] ] [ Html.text "Personal Projects" ]
         , displayRepos static.data
         ]
     }
-
-
-
---     Element.column
---         [ Element.spacing 10
---         , Element.padding 10
---         , Background.color colorMedGrey
---         , Element.width Element.fill
---         , Border.rounded 5
---         ]
---         [ Element.el [ Font.size 24 ] (Element.text repoTitle)
---         , Element.el [ Font.color colorSecondaryText ]
---             (Element.text description)
---         , Element.newTabLink
---             [ Font.color colorDisabledText
---             , Element.mouseOver [ Font.color colorPrimaryText ]
---             ]
---             { url = project.repo.htmlUrl, label = Element.text project.repo.htmlUrl }
---         , projectDetailsButton project model
---         ]
--- displayProjects : Model -> List Project -> Element Msg
--- displayProjects model projs =
---     Element.column
---         [ Element.width Element.fill
---         , Element.spacing 15
---         , Element.paddingXY 25 5
---         ]
---         (List.map
---             (displayProject model)
---             projs
---         )
--- type alias RepoName =
---     String
--- type alias Model =
---     { expanded : Dict RepoName Bool }
--- type Msg
---     = Toggle RepoName
--- type alias RouteParams =
---     {}
--- page : PageWithState RouteParams Data Model Msg
--- page =
---     Page.single
---         { head = head
---         , data = data
---         }
---         |> Page.buildWithLocalState { view = view, init = init, update = update, subscriptions = subscriptions }
--- init : Maybe PageUrl -> Shared.Model -> StaticPayload Data RouteParams -> ( Model, Cmd Msg )
--- init maybePageUrl model static =
---     let
---         allFalse =
---             List.map (\project -> ( project.repo.name, False )) static.data
---     in
---     ( { expanded = Dict.fromList allFalse }, Cmd.none )
--- update : PageUrl -> Maybe Browser.Navigation.Key -> Shared.Model -> StaticPayload Data RouteParams -> Msg -> Model -> ( Model, Cmd Msg )
--- update pageUrl maybeKey sharedModel static msg model =
---     let
---         toggleBool maybeBool =
---             case maybeBool of
---                 Just bool ->
---                     Just (not bool)
---                 Nothing ->
---                     Just False
---     in
---     case msg of
---         Toggle name ->
---             ( { model | expanded = Dict.update name toggleBool model.expanded }, Cmd.none )
--- subscriptions : Maybe PageUrl -> RouteParams -> Path -> Model -> Sub Msg
--- subscriptions maybePageUrl routeParams path model =
---     Sub.none
--- type alias Data =
---     List Project
--- data : DataSource Data
--- data =
---     projects
--- head :
---     StaticPayload Data RouteParams
---     -> List Head.Tag
--- head static =
---     Seo.summary
---         { canonicalUrlOverride = Nothing
---         , siteName = "Fixed Points"
---         , image =
---             { url = Pages.Url.external "TODO"
---             , alt = "Fixed Points logo"
---             , dimensions = Nothing
---             , mimeType = Nothing
---             }
---         , description = "Fixed Points - Projects"
---         , locale = Nothing
---         , title = "Fixed Points - Projects" -- metadata.title -- TODO
---         }
---         |> Seo.website
--- view :
---     Maybe PageUrl
---     -> Shared.Model
---     -> Model
---     -> StaticPayload Data RouteParams
---     -> View Msg
--- view maybeUrl sharedModel model static =
---     { title = "Fixed Points - Personal Projects"
---     , body = [ Element.el [ Font.size 30, Element.centerX ] (Element.text "Personal Projects"), displayProjects model static.data ]
---     }
