a8d1946cdfcb0536da480e61892cee327376bae6
Ryan Sullivant <rsulli55@gmail.com>
2021-08-08 13:04:45 -0700

    Update Site.elm and added icons

--- src/Site.elm
+++ src/Site.elm
@@ -2,7 +2,10 @@ module Site exposing (config)
 
 import DataSource
 import Head
+import MimeType
 import Pages.Manifest as Manifest
+import Pages.Url
+import Path
 import Route
 import SiteConfig exposing (SiteConfig)
 
@@ -14,7 +17,7 @@ type alias Data =
 config : SiteConfig Data
 config =
     { data = data
-    , canonicalUrl = "https://elm-pages.com"
+    , canonicalUrl = "https://fixedpoints.xyz"
     , manifest = manifest
     , head = head
     }
@@ -27,15 +30,34 @@ data =
 
 head : Data -> List Head.Tag
 head static =
-    [ Head.sitemapLink "/sitemap.xml"
+    [ Head.icon [ ( 32, 32 ) ] MimeType.Png (Pages.Url.fromPath <| Path.fromString "images/logo_white_32.png")
+    , Head.icon [ ( 16, 16 ) ] MimeType.Png (Pages.Url.fromPath <| Path.fromString "images/logo_white_16.png")
+    , Head.appleTouchIcon (Just 180) (Pages.Url.fromPath <| Path.fromString "images/logo_white_180.png")
+    , Head.appleTouchIcon (Just 192) (Pages.Url.fromPath <| Path.fromString "images/logo_white_192.png")
+    , Head.sitemapLink "/sitemap.xml"
     ]
 
 
 manifest : Data -> Manifest.Config
 manifest static =
     Manifest.init
-        { name = "Site Name"
-        , description = "Description"
+        { name = "Fixed Points"
+        , description = "Fixed Points - Ryan Sullivant's website"
         , startUrl = Route.Index |> Route.toPath
-        , icons = []
+        , icons =
+            [ icon MimeType.Png 192
+            , icon MimeType.Png 512
+            ]
         }
+
+
+icon :
+    MimeType.MimeImage
+    -> Int
+    -> Manifest.Icon
+icon format width =
+    { src = Pages.Url.fromPath <| Path.fromString ("images/logo_white_" ++ String.fromInt width ++ ".png")
+    , sizes = [ ( width, width ) ]
+    , mimeType = format |> Just
+    , purposes = [ Manifest.IconPurposeAny, Manifest.IconPurposeMaskable ]
+    }
